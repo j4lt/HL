abcc3edf429d853f42ad7ba09cb8574c80dffc5d
j4lt <99302990+j4lt@users.noreply.github.com>
2022-06-07 09:50:16 +1200

    Add files via upload

---
+++ HL_max_guesses.py
@@ -0,0 +1,41 @@
+# HL component 11 - Maximum Guesses Calculator
+
+import math
+
+for item in range(0,4):
+    low = int(input("Low: ")) # Use int check in due course
+    high = int(input("High: ")) # Use int check in due course
+
+    range = high - low + 1
+    max_raw = math.log2(range) 
+    max_upped = math.ceil(max_raw)
+    max_guesses = max_upped + 1
+    print("Max Guesses: {}".format(max_guesses))
+
+    print()
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
